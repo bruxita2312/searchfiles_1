222259a7dbd10ea5d6f95dc0722da2b3d0c360ab
daneiva <daneiva@gmail.com>
2018-01-25 12:53:58 +0000

    updating validator

--- src/com/jalasoft/search_files/utils/validator.py
+++ src/com/jalasoft/search_files/utils/validator.py
@@ -1,27 +1,32 @@
 class Validator:
 
+    def is_menu(self, menu, limit):
+        res_menu_validator = False
 
-def menu_validator(menu):
-    res_menu_validator = False
-
-    if is_only_numbers(menu):
-
-        if 4 >= menu >= 1:
-            res_menu_validator = True
+        if is_only_numbers(self, menu):
+            if limit >= menu >= 1:
+                res_menu_validator = True
+            else:
+                res_menu_validator = False
         else:
             res_menu_validator = False
-    else:
-        res_menu_validator = False
-    return res_menu_validator
+        return res_menu_validator
 
-def is_only_numbers(input):
-    {}
+    def is_only_numbers(self, value):
+        {}
 
-def is_only_text(input):
-    {}
+    def is_only_text(self, value):
+        {}
 
-def is_alphanumeric(input):
-    {}
+    def is_alphanumeric(self, value):
+        {}
 
-def empty_validation(input):
-    {}
+    def empty_validation(self, value):
+        {}
+
+    def response_status(self, response_dict):
+        status = "Pass"
+        for response in response_dict.keys():
+            if response_dict[response]['status'] == "Fail":
+                status = "Fail"
+        return status

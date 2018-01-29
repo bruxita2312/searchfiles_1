3539bd478230a7721e9ccd8b372f302fffb68c52
Daniela Ticona <daniela.ticona@gmail.com>
2018-01-29 19:39:15 -0400

    ya corre solo falta agarrar los paths y que buscar continuo en mi casa

--- src/com/jalasoft/search_files/menu/menu_maim.py
+++ src/com/jalasoft/search_files/menu/menu_maim.py
@@ -7,12 +7,12 @@ class Menu:
 
     def __init__(self):
         '''self.validator = Validator()'''
-        self.elections = {
-            1: self.search_by_name,
-            2: self.search_by_extension,
-            3: self.search_by_size,
-            4: self.quit
-        }
+        #self.elections = {
+         #   "1": self.search_by_name,
+         #   "2": self.search_by_extension,
+         #   "3": self.search_by_size,
+         #   "4": self.quit
+        #}
 
     def show_menu(self):
         print("_*_*_*_*_*_ Menu _*_*_*_*_*_")
@@ -27,20 +27,41 @@ class Menu:
         while True:
             self.show_menu()
             election = int(input("enter an option:  "))
-
-            action = self.elections.get(election, None)
-            print self.elections[election]
-
-
+            if election==1:
+                print ("you select 1")
+                Menu.search_by_name()
+                break
+            elif election == 2:
+                print("you select 2")
+                Menu.search_by_extension()
+                break
+            elif election == 3:
+                print("you select 3")
+                Menu.search_by_extension()
+                break
+            elif election == 4:
+                print("you select 4")
+                print("Bye Bye >)")
+                sys.exit(0)
+            elif  election >5 and election <= 9:
+                print("Please, introduce the valid value")
+            elif election == 0:
+                break
+            else:
+                print("miauuuuu")
+            #action = self.elections.get(election, None)
+            #print(self.elections[election])
             #DANEEEEEEEEEEEE aca necesito que me vuelva un trueeeee...helps meee#
             #if action:
-             #   action()
+            #   action()
             #else:
-             #
-                #   print("{0} option not valid".format(election))
+            #
+            #   print("{0} option not valid".format(election))
+
 
 
     def search_by_name (self):
+
         path =input("Enter path")
         '''true = self.validator.search(filter)'''
         name= input("Enter name")

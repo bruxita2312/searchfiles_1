8eeb2da5ee408b431fc5f2f976eb060534173761
Daneiva <daneiva@gmail.com>
2018-02-07 01:34:18 -0400

    updated the validator.py

--- src/com/jalasoft/search_files/utils/validator.py
+++ src/com/jalasoft/search_files/utils/validator.py
@@ -1,58 +1,72 @@
 import logging.config
 import os
 
-logging.basicConfig(filename="../../../../../log/validator.log", level=logging.DEBUG,
-                    format='%(levelname)s : %(asctime)s : %(message)s')
-from concurrent.futures._base import LOGGER
+from src.com.jalasoft.search_files.utils.logging1 import LOGGER as logger
 
 
 class Validator:
-    def __init__(self):
-        pass
-
-    def is_menu(self, menu, limit):
-        res_menu_validator = False
-
-        if is_only_numbers(self, menu):
-            if limit >= menu >= 1:
-                res_menu_validator = True
-            else:
-                res_menu_validator = False
-        else:
-            res_menu_validator = False
-        return res_menu_validator
+    logger.info("Failed Verification, the type must be a option valid: ")
 
     def is_only_numbers(self, value):
-        if type(value) != int:
-            LOGGER.error("Failed Verification, the type must be a option valid: ".format(value))
+        if type(value) != int:logger.error("Failed Verification, the type must be a option valid: ".format(value))
 
-    def is_only_text(self, value):
-        if type(value) not in (int, float, complex):
-            LOGGER.error("Failed Verification, only a text value should be insert:".format(value))
+        if __name__ == "__main__":
+            search = Validator()
+            search.is_only_numbers("fgf")
 
-    def empty_validation(self, value):
-        if value is None:
-            LOGGER.error("Empty value found.")
+        class Validator:
+            def is_only_numbers(self, value):
 
-    def is_path(self, value, intentos=0):
-        while intentos < 5:
-            if os.path.lexists(value) == True:
-                res_path = True
-                break
-            else:
-                res_path = False
-                intentos += 1
-        if intentos >= 5:
-                    LOGGER.error("Incorrect Path, try " + str(intentos) + " attempts")
-        return res_path
+                if type(value) != int:
+                    logger.error("Failed Verification, the type must be a option validddddd: ".format(value))
 
-    def is_file(self, value, intentos=0):
-        {}
+            def is_menu(self, menu, limit):
+                res_menu_validator = False
+                if is_only_numbers(self, menu):
+                    if limit >= menu >= 1:
+                        res_menu_validator = True
+                    else:
+                        res_menu_validator = False
+                else:
+                    res_menu_validator = False
+                return res_menu_validator
 
-    def response_status(self, response_dict):
-        status = "Pass"
-        for response in response_dict.keys():
-            if response_dict[response]['status'] == "Fail":
-                status = "Fail"
-        return status
+            def is_only_numbers(self, value):
+                if type(value) != int:
+                    logger.error("Failed Verification, the type must be a number: ".format(value))
 
+            def is_only_text(self, value):
+                if type(value) not in (int, float, complex):
+                    logger.error("Failed Verification, only a text value should be insert:".format(value))
+
+            def empty_validation(self, value):
+                if value is None:
+                    logger.error("Empty value found.")
+
+            def is_path(self, value, intentos=0):
+                while intentos < 5:
+                    if os.path.lexists(value) == True:
+                        res_path = True
+                        break
+                    else:
+                        res_path = False
+                        intentos += 1
+                if intentos >= 5:
+                    logger.error("Incorrect Path, try " + str(intentos) + " attempts")
+                return res_path
+
+            def is_file(self, value, intentos=0):
+
+                {}
+
+            def response_status(self, response_dict):
+                status = "Pass"
+                for response in response_dict.keys():
+                    if response_dict[response]['status'] == "Fail":
+                        status = "Fail, response status"
+                return status
+
+        if __name__ == "__main__":
+            path1 = 'C:\\Users'
+
+            validator = Validator()
